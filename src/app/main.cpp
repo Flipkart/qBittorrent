ff0aa9e9adcf9d14b7b0982e622f783bd8d07d0e
anvinjain <ankurjain@outlook.com>
2018-08-23 14:57:58 +0530

    changed debug log message pattern

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -136,7 +136,7 @@ int main(int argc, char *argv[])
     bool isOneArg = (argc == 2);
 
 #if defined(QBT_USES_QT5)
-    qSetMessagePattern("[%{if-debug}D%{endif}%{if-info}I%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}%{if-fatal}F%{endif}][%{file}:%{line} %{function}] %{message}");
+    qSetMessagePattern("[%{time yyyy-MM-dd h:mm:ss.zzz t}] [%{file}:%{line}] - %{message}")
 #endif
 
 #ifdef Q_OS_MAC
